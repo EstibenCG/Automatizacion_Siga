8bfcf1ecb5b33dca66392ae6c54980bdaebee8dd
Edwin1538 <179547472+Edwin1538@users.noreply.github.com>
2025-11-26 19:21:21 -0500

    UPDATE SuiteEditar

--- src/test/java/co/com/AutoSiga/runners/SuiteEditar/SuiteEditar.java
+++ src/test/java/co/com/AutoSiga/runners/SuiteEditar/SuiteEditar.java
@@ -1,12 +1,14 @@
 package co.com.AutoSiga.runners.SuiteEditar;
 
+import co.com.AutoSiga.runners.EditarAulaRunner;
 import co.com.AutoSiga.runners.EditarUsuarioRunner;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        EditarUsuarioRunner.class
+        EditarUsuarioRunner.class,
+        EditarAulaRunner.class
 })
 public class SuiteEditar {
 
